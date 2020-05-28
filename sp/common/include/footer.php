c695c5f8749c716533ca7a60f7dc4b4515d3830a
yanagi <yanagida@wadakoubou.com>
2020-05-28 17:56:58 +0900

    aaa

--- sp/common/include/footer.php
+++ sp/common/include/footer.php
@@ -1,4 +1,41 @@
 <img src="/sp/common/img/area.png" width="100%" alt=""/>
+<!--insta-->	
+  <div class="instaBlock">
+  <h2>
+    <img src="/sp/img/main_insta.png" alt="">
+  </h2>
+	<ul id="instafeed"></ul>
+  </div>
+  <script>
+
+    $(function(){
+	$.ajax({
+		type: 'GET',
+		url: 'https://graph.facebook.com/v3.0/17841433755873097?fields=name%2Cmedia.limit(10)%7Bcaption%2Clike_count%2Cmedia_url%2Cmedia_type%2Cthumbnail_url%2Cpermalink%2Ctimestamp%2Cusername%7D&access_token=EAAIg4gAnwZCoBAA4KuVgkmhJU2wFajoXqGGVm8DcuMWpBz5XbPbAyS8SA91K2WpxqRMeJfy59seHGiMxxm1veZB9vuWJiVP0a92unxrQNx4VhSj1owJCO1TrY1mZBshHDZBkvw2kZAeOEhMin8edAKK0XCq4RuvKbSZB3KLFwERkhTzZAGhv0ht8u5E1FaUm68ZD',
+		dataType: 'json',
+		success: function(json) {
+		    	
+		    var html = '';
+		    var insta = json.media.data;
+		    for (var i = 0; i < insta.length; i++) {
+		    var media_type = insta[i].media_type;
+                if ( insta[i].media_type == "IMAGE" || insta[i].media_type == "CAROUSEL_ALBUM" ) {
+		    	html += '<li><a href="' + insta[i].permalink + '" target="_blank" rel="noopener noreferrer"><span class="square-content"><img src="' + insta[i].media_url + '"></span></a></li>';                
+                } else if (media_type == "VIDEO" ) {
+		    	html += '<li><a href="' + insta[i].permalink + '" target="_blank" rel="noopener noreferrer"><span class="square-content"><img src="' + insta[i].thumbnail_url + '"></span></a></li>';           
+		    var media_type = '';                    
+                }       
+		    }
+		      $("#instafeed").append(html);			
+		},
+		error: function() {
+ 
+		//エラー時の処理
+		}
+	});
+});	
+</script>
+<!--insta-->
 <div id="footer">
 		<div class="footInner"> 
 			<p>カーペット・椅子ソファークリーニングなら株式会社タック <br>
